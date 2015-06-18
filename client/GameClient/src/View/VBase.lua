203bc9df32d0ddbe30c891cd89d4c3e0f1bbf433
xzben <994344450@qq.com>
2015-06-18 14:54:28 +0800

    增加keyback 功能和 音频管理器

--- client/GameClient/src/View/VBase.lua
+++ client/GameClient/src/View/VBase.lua
@@ -9,5 +9,46 @@
 VBase = VBase or class("VBase", EventDispatcher)
 
 function VBase:ctor()
-	
+    
+	local function handlercallback(event)
+        if "enter" == event then
+            self:root_on_enter()
+        elseif "exit" == event then
+            self:root_on_exit()
+        end
+    end
+    self:registerScriptHandler(handlercallback)
+end
+
+function VBase:root_on_enter()
+    print("VBase on_enter")
+    
+    if self.handleKeyBackClicked then
+        print("VBase:root_on_enter()")
+        GSession:registerKeybackListener(self.handleKeyBackClicked, self)
+    end
+
+    if self.handleKeyMenuClicked then
+        GSession:registerKeyMenuListener(self.handleKeyMenuClicked, self)
+    end
+
+    if self.on_enter then
+        self:on_enter()
+    end
+end
+
+function VBase:root_on_exit()
+    print("VBase on_exit")
+    
+    if self.handleKeyBackClicked then
+        GSession:unregisterKeybackListener(self.handleKeyBackClicked, self)
+    end
+
+    if self.handleKeyMenuClicked then
+        GSession:unregisterKeyMenuListener(self.handleKeyMenuClicked, self)
+    end
+
+    if not self.on_exit then
+        self:on_exit()
+    end
 end
