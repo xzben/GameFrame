2bf637eebefacdc977d0afdc57b81bef1c21dc3c
xzben <994344450@qq.com>
2015-06-18 14:54:28 +0800

    增加keyback功能和audio

--- client/GameClient/src/GSession.lua
+++ client/GameClient/src/GSession.lua
@@ -5,7 +5,6 @@
 --
 -- 本文见存放整个游戏的控制逻辑
 -------------------------------------------------------------------------------
-
 require_ex("Data.MPlayer")
 
 Session = Session or class("Session", VBase)
@@ -28,8 +27,11 @@ function Session:ctor()
     self._needRemoveUnusedCached = false
     self._nowRemoveUnusedCached = false
 	self._curRunningScene = nil
+    self._keypadbackListener = {}
+    self._keypadmenuListener = {}
 
     self._player = MPlayer.new()
+    self._audioManager = nil
 	self:init()
 end
 
@@ -73,8 +75,9 @@ function Session:runWithScene( newScene )
 end
 
 function Session:init()
-	self:initFileUtils();
+    self:initFileUtils();
 	self:initDirector();
+    self:registerKeypadManager()
 
     local function update(dt)
         self:update(dt)
@@ -88,11 +91,14 @@ end
 
 function Session:lauchScene()
     require_ex("View.LauchScene.LauchScene")
-	if director:getRunningScene() then
+    --require_ex("Audio.AudioManager")
+    if director:getRunningScene() then
         director:replaceScene(self)
     else
         director:runWithScene(self)
     end
+    
+    --self._audioManager = AudioManager.create()
     
     local scene = LauchScene.create()
     if scene then
@@ -100,6 +106,7 @@ function Session:lauchScene()
     end
 end
 
+---------------------------内存资源释放 相关管理逻辑 -------------------------------------------
 function Session:setNeedToRemoveUnusedCached( isNeedRemove )
     self._needRemoveUnusedCached = isNeedRemove
 end
@@ -117,6 +124,65 @@ function Session:update(dt)
         self._needRemoveUnusedCached = false
     end
 end
+---------------------------keypad 相关管理逻辑 ----------------------------------------------
+function Session:registerKeypadManager()
+    local function onKeyReleased(keyCode, event)
+        if keyCode == cc.KeyCode.KEY_BACK then
+            self:handleKeybackClicked()
+        elseif keyCode == cc.KeyCode.KEY_MENU  then
+            self:handleKeyMenuClicked()
+        end
+    end
+    local listener = cc.EventListenerKeyboard:create()
+    listener:registerScriptHandler(onKeyReleased, cc.Handler.EVENT_KEYBOARD_RELEASED )
+    local eventDispatcher = self:getEventDispatcher()
+    eventDispatcher:addEventListenerWithSceneGraphPriority(listener, self)
+    self._keypadbackListener = {}
+    self._keypadmenuListener = {}
+end
 
+function Session:registerKeybackListener(func, owner)
+    if not func then return end
+    table.insert(self._keypadbackListener, 1, { func = func, owner = owner} )
+end
+
+function Session:unregisterKeybackListener(func, owner)
+    if not func then return end
+    local listener = table.remove(self._keypadbackListener, 1)
+    assert(listener.func == func and listener.owner == owner, "Session:unregisterKeybackListener unregister a no match")
+end
+
+function Session:registerKeyMenuListener(func, owner)
+    if not func then return end
+    table.insert(self._keypadmenuListener, 1, { func = func, owner = owner} )
+end
+
+function Session:unregisterKeyMenuListener(func, owner)
+    if not func then return end
+    local listener = table.remove(self._keypadmenuListener, 1)
+    assert(listener.func == func and listener.owner == owner, "Session:unregisterKeybackListener unregister a no match")
+end
+
+function Session:handleKeybackClicked()
+    local listener = self._keypadbackListener[1]
+    if listener then
+        listener.func(listener.owner)
+    end
+end
+
+function Session:handleKeyMenuClicked()
+    local listener = self._keypadmenuListener[1]
+    if listener then
+        listener.func(listener.owner)
+    end
+end
+-----------------------------------------------------------------------------------------
 GSession = GSession or Session.create()
 
+function RequestEvent( event, ...)
+    GSession:dispatch_event(event, ...)
+end
+
+function HandleRequestEvent(event, func, ower)
+    GSession:add_listener(event, func, ower)
+end
--- client/GameClient/src/View/VBase.lua
+++ client/GameClient/src/View/VBase.lua
@@ -15,6 +15,7 @@ function createSpriteMenuItem(img, rect, callback, obj)
     
     local item = cc.MenuItemSprite:create(sprite1, sprite2);
     local function itemcallback()
+        RequestEvent("playEffect", "btnClick")
         callback(obj, item)
     end
     item:registerScriptTapHandler(itemcallback)
@@ -23,5 +24,46 @@ function createSpriteMenuItem(img, rect, callback, obj)
 end
 
 function VBase:ctor()
-	
+    
+	local function handlercallback(event)
+        if "enter" == event then
+            self:root_on_enter()
+        elseif "exit" == event then
+            self:root_on_exit()
+        end
+    end
+    self:registerScriptHandler(handlercallback)
+end
+
+function VBase:root_on_enter()
+    print("VBase on_enter")
+    
+    if self.handleKeyBackClicked then
+        print("VBase:root_on_enter()")
+        GSession:registerKeybackListener(self.handleKeyBackClicked, self)
+    end
+
+    if self.handleKeyMenuClicked then
+        GSession:registerKeyMenuListener(self.handleKeyMenuClicked, self)
+    end
+
+    if self.on_enter then
+        self:on_enter()
+    end
+end
+
+function VBase:root_on_exit()
+    print("VBase on_exit")
+    
+    if self.handleKeyBackClicked then
+        GSession:unregisterKeybackListener(self.handleKeyBackClicked, self)
+    end
+
+    if self.handleKeyMenuClicked then
+        GSession:unregisterKeyMenuListener(self.handleKeyMenuClicked, self)
+    end
+
+    if not self.on_exit then
+        self:on_exit()
+    end
 end
